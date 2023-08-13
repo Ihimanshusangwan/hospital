be5ddbc335da2c620d7ff9fe876fcc24913511b2
Himanshu sharma <hs991009@gmail.com>
2023-08-13 19:31:54 +0530

    all chnages till case sheet audit done

--- staff/bloodTransfusion.php
+++ staff/bloodTransfusion.php
@@ -95,7 +95,7 @@ if (isset($_POST['submit'])) {
 
 
 }
-error_reporting(0);
+error_reporting(0); 
 $sql3 = "SELECT * FROM `blood`  WHERE id = '$id';";
 $data3 = $conn->query($sql3);
 $blood = $data3->fetch_assoc();
